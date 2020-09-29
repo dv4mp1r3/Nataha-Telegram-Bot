98666e015b94c5f2b7c228fef43c93a8ee7010ea
dv4mp1r3 <somebody@somewhere>
2020-09-29 13:17:27 +0300

    временное отключение неиспользуемого бота из точки входа

--- ep.php
+++ ep.php
@@ -1,6 +1,6 @@
 <?php
 
-use Bots\TelegramNeVsratoslavBot;
+use Bots\TelegramSecurityExpertBot;
 use Commands\CommandListener;
 use Commands\HashIdCommand;
 use Misc\Application;
@@ -11,18 +11,16 @@ require_once './config.php';
 require_once './vendor/autoload.php';
 
 $reader = defined('IS_DEBUG') && IS_DEBUG
-    ? (new FileReader(__DIR__.'/input/reply_on_image.json'))
+    ? (new FileReader(__DIR__.'/input/text_chat.json'))
     : (new PhpInputReader());
-$db = new \PDO(PDO_MEME_DSN);
-$db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+//$db = new \PDO(PDO_MEME_DSN);
+//$db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
 (new Application(
-    (new TelegramNeVsratoslavBot(
+    (new TelegramSecurityExpertBot(
         (new CommandListener())
         ->addCommand('/hashid', new HashIdCommand()),
         $reader
-    ))->setFontPath(__DIR__.'/lobster.ttf')
-        ->setMemeTextQuery(PDO_MEME_QUERY)
-        ->setMemTextPdo($db),
+    )),
     defined('IS_DEBUG') && IS_DEBUG
 )
 )->run();
