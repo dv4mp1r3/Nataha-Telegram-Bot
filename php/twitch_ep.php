7c7628bb174df1ebb37a4c5c1ae2868dc9ff9257
dv4mp1r3 <somebody@somewhere>
2021-10-30 23:18:44 +0300

    добавление флага в .env для опциональной обработки события

--- php/twitch_ep.php
+++ php/twitch_ep.php
@@ -10,20 +10,21 @@ require_once './config.php';
 require_once './vendor/autoload.php';
 
 $logger = new Logger();
-(new Application(
-    (new TwitchSecurityExpertBot(
-        TWITCH_DEFAULT_SERVER,
-        TWITCH_DEFAULT_PORT,
-        getenv('TWITCH_USERNAME'),
-        getenv('TWITCH_PASSWORD'),
-        [getenv('TWITCH_CHANNEL_NAME')],
-        CONFIG_PATH))
-        ->setEvent(\Bots\SocketBot::BEFORE_SEND_EVENT,
-            new \Bots\Events\TwitchBeforeSendEvent(
-                "http://node:3000/audio",
-                getenv('YA_CLOUD_TOKEN'),
-                getenv('YA_CLOUD_FOLDER'))
-        ),
-    $logger,
-    defined('IS_DEBUG') && IS_DEBUG
-))->run();
+$bot = new TwitchSecurityExpertBot(
+    TWITCH_DEFAULT_SERVER,
+    TWITCH_DEFAULT_PORT,
+    getenv('TWITCH_USERNAME'),
+    getenv('TWITCH_PASSWORD'),
+    [getenv('TWITCH_CHANNEL_NAME')],
+    CONFIG_PATH
+);
+if (intval(getenv('USE_DISCORD')) === 1) {
+    $bot->setEvent(
+    \Bots\SocketBot::BEFORE_SEND_EVENT,
+    new \Bots\Events\TwitchBeforeSendEvent(
+        "http://node:3000/audio",
+        getenv('YA_CLOUD_TOKEN'),
+        getenv('YA_CLOUD_FOLDER'))
+    );
+}
+(new Application($bot, $logger, defined('IS_DEBUG') && IS_DEBUG))->run();
